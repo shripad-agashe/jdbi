d02dc9ca75a48ae692d829548cd39c0d9a73cf2d
Steven Schlansker <sschlansker@opentable.com>
2018-11-29 18:22:54 -0800

    Does Oracle work now?

--- src/build/ci-install.sh
+++ src/build/ci-install.sh
@@ -3,9 +3,4 @@ set -xe
 
 OPTS="-DskipTests=true -Dbasepom.check.skip-all=true -Dmaven.javadoc.skip=true -B"
 
-# Oracle profile disabled until they fix their orai18n JAR artifact on maven.oracle.com repository.
-#if [ "$TRAVIS_SECURE_ENV_VARS" == "true" ]; then
-#  PROFILES="$PROFILES,oracle"
-#fi
-
-exec mvn ${OPTS} -P${PROFILES} verify
+exec mvn ${OPTS} -Poracle verify
--- src/build/ci-test.sh
+++ src/build/ci-test.sh
@@ -3,9 +3,4 @@ set -xe
 
 OPTS="-Dmaven.javadoc.skip=true -Dbasepom.check.skip-basic=true -Dbasepom.check.skip-findbugs=true -Dbasepom.check.skip-pmd=true -Dbasepom.check.skip-checkstyle=true -B"
 
-# Oracle profile disabled until they fix their orai18n JAR artifact on maven.oracle.com repository.
-#if [ "$TRAVIS_SECURE_ENV_VARS" == "true" ]; then
-#  PROFILES="$PROFILES,oracle"
-#fi
-
-exec mvn ${OPTS} -P${PROFILES} verify
+exec mvn ${OPTS} -Poracle verify
