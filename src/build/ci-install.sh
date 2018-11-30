e2427ce3ac4e1d68b461e3248269efe9d5d31be3
Steven Schlansker <sschlansker@opentable.com>
2018-11-29 18:10:22 -0800

    Disable toolchains for ci so our openjdk matrix has a chance at working

--- src/build/ci-install.sh
+++ src/build/ci-install.sh
@@ -2,8 +2,6 @@
 
 OPTS="-DskipTests=true -Dbasepom.check.skip-all=true -Dmaven.javadoc.skip=true -B"
 
-PROFILES="toolchains"
-
 # Oracle profile disabled until they fix their orai18n JAR artifact on maven.oracle.com repository.
 #if [ "$TRAVIS_SECURE_ENV_VARS" == "true" ]; then
 #  PROFILES="$PROFILES,oracle"
